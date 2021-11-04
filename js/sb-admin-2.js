d31107da36cdd7590b47a74737ac571bb080ceb9
Maulik Soni <mausoni@microsoft.com>
2021-11-03 21:49:57 -0500

    updated2

--- js/sb-admin-2.js
+++ js/sb-admin-2.js
@@ -1,8 +1,20 @@
-(function($) {
+(function ($) {
   "use strict"; // Start of use strict
 
+  // Show connectivty layer
+  $("#connectivity01").on('click', function (e) {
+    $("#archlabel").text("Connectivity Layer");
+    $("#archimg").attr("src", "./img/NetworkLayer.JPG");
+  });
+
+  // Show secuirty layer
+  $("#security01").on('click', function (e) {
+    $("#archlabel").text("Network Layer");
+    $("#archimg").attr("src", "./img/Security.JPG");
+  });
+
   // Toggle the side navigation
-  $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
+  $("#sidebarToggle, #sidebarToggleTop").on('click', function (e) {
     $("body").toggleClass("sidebar-toggled");
     $(".sidebar").toggleClass("toggled");
     if ($(".sidebar").hasClass("toggled")) {
@@ -11,11 +23,11 @@
   });
 
   // Close any open menu accordions when window is resized below 768px
-  $(window).resize(function() {
+  $(window).resize(function () {
     if ($(window).width() < 768) {
       $('.sidebar .collapse').collapse('hide');
     };
-    
+
     // Toggle the side navigation when window is resized below 480px
     if ($(window).width() < 480 && !$(".sidebar").hasClass("toggled")) {
       $("body").addClass("sidebar-toggled");
@@ -25,7 +37,7 @@
   });
 
   // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
-  $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
+  $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function (e) {
     if ($(window).width() > 768) {
       var e0 = e.originalEvent,
         delta = e0.wheelDelta || -e0.detail;
@@ -35,7 +47,7 @@
   });
 
   // Scroll to top button appear
-  $(document).on('scroll', function() {
+  $(document).on('scroll', function () {
     var scrollDistance = $(this).scrollTop();
     if (scrollDistance > 100) {
       $('.scroll-to-top').fadeIn();
@@ -45,7 +57,7 @@
   });
 
   // Smooth scrolling using jQuery easing
-  $(document).on('click', 'a.scroll-to-top', function(e) {
+  $(document).on('click', 'a.scroll-to-top', function (e) {
     var $anchor = $(this);
     $('html, body').stop().animate({
       scrollTop: ($($anchor.attr('href')).offset().top)
